63ebb91db6164542c38f8a34a2bc44b85688a8e1
Hector Rodriguez <riosrodriguezhectorhernan59@gmail.com>
2026-01-22 19:42:21 -0500

    feat(register): implement user registration with validation and localStorage, sessionStorage logic

--- js/auth.js
+++ js/auth.js
@@ -1,12 +1,77 @@
-import { findUser } from './storage.js';
+import * as Storage from './storage.js';
 
+// Login
+const registerForm = document.getElementById('signup-form');
+
+if (registerForm) {
+  registerForm.addEventListener('submit', registerUser);
+}
+
+function registerUser(e) {
+  e.preventDefault();
+
+  // Select Data
+  const userName = document.getElementById('nameUser').value.trim();
+  const email = document.getElementById('email-sign').value.trim();
+  const password = document.getElementById('password-sign').value;
+  const confirmPassword = document.getElementById('confirm-password').value;
+  const message = document.getElementById('signUp-message');
+
+  message.innerText = '';
+  message.style.color = '';
+
+  // Validation password
+  if (password !== confirmPassword) {
+    message.innerText = 'Passwords do not match';
+    message.style.color = 'red';
+    return;
+  }
+
+  const users = Storage.getUsers();
+
+  // Validation user exists
+  if (users.some((u) => u.email.toLowerCase() === email.toLowerCase())) {
+    message.innerText = 'The user already exists';
+    message.style.color = 'red';
+    return;
+  }
+
+  // Hashtags
+  const defaultHashtags = [
+    'programacion',
+    'salud',
+    'videojuegos',
+    'series_o_peliculas',
+    'ideas_creativas',
+  ];
+
+  // New User
+  const newUser = {
+    id: crypto.randomUUID(),
+    name: userName,
+    email,
+    password,
+    hashtags: [...defaultHashtags],
+  };
+
+  Storage.saveUser(newUser);
+  Storage.setCurrentUser(newUser);
+
+  message.innerText = 'Account created successfully!';
+  message.style.color = '#10b981';
+
+  setTimeout(() => {
+    window.location.href = 'ideas.html'; // Redirect to ideas.html
+  }, 1000);
+}
+
+// Login
 const loginForm = document.getElementById('login-form');
 
 if (loginForm) {
   loginForm.addEventListener('submit', loginUser);
 }
 
-// Login
 function loginUser(e) {
   e.preventDefault();
 
@@ -15,7 +80,7 @@ function loginUser(e) {
   const password = document.getElementById('password').value;
   const message = document.getElementById('login-message');
 
-  const user = findUser(email, password);
+  const user = Storage.findUser(email, password);
 
   // Validation
   if (user) {
